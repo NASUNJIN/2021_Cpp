d499e50381fd13e0eb8d5de5647f73ec1495d209
NASUNJIN <sj010618@gmail.com>
2021-09-22 19:22:56 +0900

    Create HW1_3.cpp

---
+++ HW1/HW1_3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Integer {
+private:
+	int a;
+public:
+	Integer(int b) {
+		a = b;
+
+	}
+	Integer(string s) {
+		a = stoi(s);
+	}
+	int get() {
+		return a;
+	}
+	int set(int b) {
+		return a = b;
+	}
+	int isEven() {
+		return true;
+	}
+};
+
+int main() {
+	Integer n(30);
+	cout << n.get() << ' '; // 30 출력
+	n.set(50);
+	cout << n.get() << ' '; // 50 cnffur
+
+	Integer m("300");
+	cout << m.get() << ' '; //300 출력
+	cout << m.isEven(); // true(정수로 1) 출력
+}
