f609f396d73b29e01b8b0583b23195783a101051
NASUNJIN <sj010618@gmail.com>
2021-10-03 00:03:37 +0900

    HW2_2(4장3번)

---
+++ HW2/HW2_2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	cout << "20201529 나선진" << endl << endl;
+	string text;
+	cout << "문자열 입력>>";
+	getline(cin, text, '\n');
+	
+	int sum = 0;
+	//3-1
+	for (int i = 0; i < text.length(); i++) {  //sizeof(text)
+		if(text.at(i) == 'a') {
+			sum++;
+		}
+	}
+
+	cout << "문자 a는 " << sum << "개 있습니다." << endl;
+	// 3-2
+	int index = 0;
+	int count = 0;
+	while (true) {
+		if (text.find('a', index) == -1) break;
+		index = text.find('a', index) + 1;
+		count++;
+	}
+	cout << "문자 a는 " << sum << "개 있습니다." << endl;
+	
+}
