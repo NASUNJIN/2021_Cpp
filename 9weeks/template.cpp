703795f295f704bb4ca86c63dc3009e275822c76
NASUNJIN <sj010618@gmail.com>
2021-11-23 00:40:55 +0900

    [수정] template 추가 (선진)

---
+++ 9weeks/template.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+void myswap(T& a, T& b) {
+	T tmp;
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+int main() {
+	int a = 4, b = 5;
+	myswap(a, b);
+	cout << a << ' ' << b << endl;
+
+	double c = 1.1, d = 2.3;
+	myswap(c, d);
+	cout << c << ' ' << d << endl;
+
+	char e = 'z', f = 'x';
+	myswap(e, f);
+	cout << e << ' ' << f << endl;
+}
